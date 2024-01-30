2c4c16202a501296ca05c8122f0650a7c4be53d8
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-01-30 03:23:22 +0100

    made tour section dynamic by fetching from data.js

--- backroads/src/components/Tours.js
+++ backroads/src/components/Tours.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Title from "./Title";
+import { tours } from "../data";
 
 function Tours() {
   return (
@@ -7,124 +8,35 @@ function Tours() {
       <Title title="Featured" subTitle="tours" />
       {/* <!-- Tour section container --> */}
       <div className="tour-container">
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="images/tour-1.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">August 26th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Tibet Adventure</h4>
-              <p>
-                Embark on a soul-stirring journey to the mystical land of Tibet.
-                Traverse through breathtaking landscapes, ancient monasteries,
-                and immerse yourself in the rich Tibetan culture. This 6-day
-                adventure promises an unforgettable experience, blending
-                spirituality with awe-inspiring natural beauty.
-              </p>
-            </div>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                china
-              </p>
-              <p>6 days</p>
-              <p>from $2100</p>
-            </div>
-          </div>
-        </article>
-        
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="images/tour-2.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">October 1st, 2020</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Best of Java</h4>
-              <p>
-                Discover the enchanting beauty of Java, Indonesia's cultural
-                heart. This 11-day tour takes you through ancient temples, lush
-                landscapes, and vibrant markets. Immerse yourself in the rich
-                Javanese traditions, savor local delicacies, and create lasting
-                memories in this diverse and captivating destination.
-              </p>
-            </div>
-            
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                Indonesia
-              </p>
-              <p>11 days</p>
-              <p>from $1400</p>
-            </div>
-          </div>
-        </article>
-        
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="images/tour-1.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">August 26th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Explore Hong Kong</h4>
-              <p>
-                Uncover the magic of Hong Kong in 6 days. From dazzling
-                cityscapes to hidden gems, this tour encapsulates the essence of
-                Hong Kong's diverse attractions. Immerse yourself in local
-                culture, indulge in delectable cuisine, and create lasting
-                memories.
-              </p>
-            </div>
-            
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                china
-              </p>
-              <p>8 days</p>
-              <p>from $5000</p>
-            </div>
-          </div>
-        </article>
-
-        <article className="tour-card">
-          <div className="tour-img-container">
-            <img src="images/tour-1.jpeg" className="tour-img" alt="" />
-            <p className="tour-date">August 26th, 2020</p>
-          </div>
-          <div className="tour-info">
-            <div className="tour-title">
-              <h4>Kenya Highlights</h4>
-              <p>
-                Embark on a mesmerizing journey through the heart of Kenya. This
-                6-day tour is designed to showcase the breathtaking landscapes,
-                diverse wildlife, and rich cultural heritage of this East
-                African gem. Witness the Great Migration, explore national
-                parks, and engage with local communities for an unforgettable
-                safari experience.
-              </p>
-            </div>
-            <div className="tour-footer">
-              <p>
-                <span>
-                  <i className="fas fa-map"></i>
-                </span>{" "}
-                china
-              </p>
-              <p>20 days</p>
-              <p>from $3500</p>
-            </div>
-          </div>
-        </article>
+        {tours.map((tour) => {
+          const {id, image, date, title, info, location, duration, price} = tour
+          return (
+            <article className="tour-card" key={id}>
+              <div className="tour-img-container">
+                <img src={image} className="tour-img" alt="" />
+                <p className="tour-date">{date}</p>
+              </div>
+              <div className="tour-info">
+                <div className="tour-title">
+                  <h4>{title}</h4>
+                  <p>
+                    {info}
+                  </p>
+                </div>
+                <div className="tour-footer">
+                  <p>
+                    <span>
+                      <i className="fas fa-map"></i>
+                    </span>{" "}
+                    {location}
+                  </p>
+                  <p>6 days</p>
+                  <p>from {price}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
--- backroads/src/data.js
+++ backroads/src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -21,7 +26,7 @@ export const services = [
   {
     id: 2,
     icon: "fas fa-tree fa-fw",
-    title: "Endless Hiking<",
+    title: "Endless Hiking",
     text: "Explore breathtaking trails and scenic landscapes with our diverse hiking options suitable for all skill levels.",
   },
   {
@@ -29,5 +34,48 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "Saving Money",
     text: "  Experience comfort like never before with cozy lodgings, well-appointed rooms, and attentive service.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "August 26th, 2020",
+    title: "Tibet Adventure",
+    info: " Embark on a soul-stirring journey to the mystical land of Tibet.Traverse through breathtaking landscapes, ancient monasteries, and immerse yourself in the rich Tibetan culture. This 6-day adventure promises an unforgettable experience, blending spirituality with awe-inspiring natural beauty.",
+    location: " china",
+    duration: "6 days",
+    price: "$2100",
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "October 1st, 2020",
+    title: "Best of Java",
+    info: "Discover the enchanting beauty of Java, Indonesia's cultural heart. This 11-day tour takes you through ancient temples, lush landscapes, and vibrant markets. Immerse yourself in the rich Javanese traditions, savor local delicacies, and create lasting memories in this diverse and captivating destination.",
+    location: "Indonesia",
+    duration: "11 days",
+    price: "$1400",
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "November 15th, 2020",
+    title: "Explore Hong Kong",
+    info: " Uncover the magic of Hong Kong in 6 days. From dazzling city scapes to hidden gems, this tour encapsulates the essence of Hong Kong's diverse attractions. Immerse yourself in local culture, indulge in delectable cuisine, and create lasting memories.",
+    location: " china",
+    duration: "8 days",
+    price: "$5000",
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "December 31st 2020",
+    title: "Kenya Highlights",
+    info: "   Embark on a mesmerizing journey through the heart of Kenya. This 6-day tour is designed to showcase the breathtaking landscapes, diverse wildlife, and rich cultural heritage of this East African gem. Witness the Great Migration, explore national parks, and engage with local communities for an unforgettable safari experience.",
+    location: "Kenya",
+    duration: "20 days",
+    price: "$3500",
   },
 ];
