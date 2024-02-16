abd99bdef05536f88466e2045ad9754ffdcb6df3
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-02-16 04:10:20 +0100

    refactored navbar component code

--- backroads/src/components/Navbar.js
+++ backroads/src/components/Navbar.js
@@ -1,49 +1,72 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.svg";
 import { pageLinks } from "../data";
 import { socialLinks } from "../data";
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMobileMenuOpen: false,
-    };
-  }
-
-  toggleMobileMenu = () => {
-    this.setState((prevState) => ({
-      isMobileMenuOpen: !prevState.isMobileMenuOpen,
-    }));
+const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen((prev) => !prev);
+  };
+  
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
   };
 
-  closeMobileMenu = () => {
-    this.setState({ isMobileMenuOpen: false });
-  };
-
-  render() {
-    const { isMobileMenuOpen } = this.state;
-
-    return (
-      <nav>
-        <div className="logo">
-          <a href="#">
-            <img src={logo} alt="" />
-          </a>
-        </div>
-        <ul className="nav-links">
+  return (
+    <nav>
+      <div className="logo">
+        <a href="#">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+      <ul className="nav-links">
+        {pageLinks.map((link) => (
+          <li key={link.id}>
+            <a href={link.href} className="nav-items" onClick={closeMobileMenu}>
+              {link.text}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="nav-icons">
+        {socialLinks.map((link) => (
+          <li key={link.id}>
+            <a href={link.href} target="_blank" className="nav-icon">
+              <i className={link.icon}></i>
+            </a>
+          </li>
+        ))}
+      </ul>
+      <button
+        id="menu-btn"
+        className={`hamburger ${isMobileMenuOpen ? "open" : ""}`}
+        type="button"
+        onClick={toggleMobileMenu}
+      >
+        <span className="hamburger-top"></span>
+        <span className="hamburger-middle"></span>
+        <span className="hamburger-bottom"></span>
+      </button>
+      <div
+        className={`mobile-menu ${isMobileMenuOpen ? "open" : ""}`}
+        id="menu"
+      >
+        <ul className="mobile-nav-links">
           {pageLinks.map((link) => (
             <li key={link.id}>
               <a
                 href={link.href}
                 className="nav-items"
-                onClick={this.closeMobileMenu}
+                onClick={closeMobileMenu}
               >
                 {link.text}
               </a>
             </li>
           ))}
         </ul>
+        
         <ul className="nav-icons">
           {socialLinks.map((link) => (
             <li key={link.id}>
@@ -53,53 +76,9 @@ class Navbar extends Component {
             </li>
           ))}
         </ul>
-        <button
-          id="menu-btn"
-          className={`hamburger ${isMobileMenuOpen ? "open" : ""}`}
-          type="button"
-          onClick={this.toggleMobileMenu}
-        >
-          <span className="hamburger-top"></span>
-          <span className="hamburger-middle"></span>
-          <span className="hamburger-bottom"></span>
-        </button>
-        <div
-          className={`mobile-menu ${isMobileMenuOpen ? "open" : ""}`}
-          id="menu"
-        >
-          <ul className="mobile-nav-links">
-            {pageLinks.map((link) => {
-              const { id, href, text } = link;
-              return (
-                <li key={id}>
-                  <a
-                    href={href}
-                    className="nav-items"
-                    onClick={this.closeMobileMenu}
-                  >
-                    {text}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-
-          <ul className="nav-icons">
-            {socialLinks.map((link) => {
-              const { id, href, icon } = link;
-              return (
-                <li key={id}>
-                  <a href={href} target="_blank" className="nav-icon">
-                    <i className={icon}></i>
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </nav>
-    );
-  }
-}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
